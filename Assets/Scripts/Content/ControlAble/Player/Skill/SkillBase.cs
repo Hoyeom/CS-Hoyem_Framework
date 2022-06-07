d4fd7ac9acffa6ef018d84b53d7cee9f214ad97b
Hoyeom <korehdrb@gmail.com>
2022-06-07 17:29:02 +0900

    Skill Base

---
+++ Assets/Scripts/Content/ControlAble/Player/Skill/SkillBase.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using UnityEngine;
+
+namespace Content
+{
+    public class SkillBase
+    {
+        protected SkillInfo _info;
+        public SkillInfo Info => _info;
+        
+        protected delegate void SkillChain();
+        protected SkillChain TriggerChain { get; set; }
+
+        private float _coolDownTimer = 0;
+        public event Action<float> OnChangeCoolDown;
+
+        private float CoolDown
+        {
+            get => _coolDownTimer;
+            set
+            {
+                _coolDownTimer = value;
+                OnChangeCoolDown?.Invoke(_coolDownTimer);
+            }
+        }
+        
+        public void Initialize(SkillInfo info)
+        {
+            _info = info;
+            SkillInit();
+        }
+
+        /// <summary>
+        /// TriggerChain 작성
+        /// </summary>
+        protected void SkillInit()
+        {
+            _coolDownTimer = _info.CoolDown;
+            
+            switch (_info.Trigger)
+            {
+                case SkillInfo.SkillTrigger.Press:
+                    TriggerChain += OnPress;
+                    break;
+                case SkillInfo.SkillTrigger.Hold:
+                    TriggerChain += OnHold;
+                    break;
+                case SkillInfo.SkillTrigger.Passive:
+                    TriggerChain += OnPassive;
+                    break;
+            }
+        }
+
+        public void OnUpdate()
+        {
+            CoolDown -= Time.deltaTime;
+        }
+
+        public void Active()
+        {
+            TriggerChain?.Invoke();
+        }
+
+        private void OnPress()
+        {
+            
+        }
+
+        private void OnHold()
+        {
+            
+        }
+
+        private void OnPassive()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Content/ControlAble/Player/Skill/SkillInfo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Content
+{
+    [CreateAssetMenu(menuName = "SkillInfo", fileName = "NewSkillInfo")]
+    public class SkillInfo : ScriptableObject
+    {
+        public enum SkillTrigger
+        {
+            Press,
+            Hold,
+            Passive
+        }
+
+        [SerializeField] private SkillTrigger trigger;
+        [SerializeField] private float coolDown;
+        [SerializeField] private AnimationClip clip;
+
+        public SkillTrigger Trigger { get => trigger; }
+        public float CoolDown => coolDown;
+        public AnimationClip Clip => clip;
+    }
+}
